c741722cb7d813ba0e69c6d22749247440f81cf6
Ninte <ninte.dev@gmail.com>
2020-12-14 14:49:06 +0100

    add code highlighing for loop construction in dart

--- main.dart
+++ main.dart
@@ -13,11 +13,32 @@
 //   print(cubes);
 // }
 
+// main() {
+//   var temp = {6, 8, 23, 97, 10};
+//   var temp2 = {8, 25, 7, 1, 90, 34, 23, 10};
+
+//   var intersectionSet = temp.union(temp2);
+
+//   print(intersectionSet);
+// }
+
 main() {
-  var temp = {6, 8, 23, 97, 10};
-  var temp2 = {8, 25, 7, 1, 90, 34, 23, 10};
+  // You have to create and populate a list of integers using an already existing list.
+  // Multiply each item in the list with 3.
+  // The items of the final list should only contain even numbers.
+  var evenList = List();
+// Write your code here
 
-  var intersectionSet = temp.union(temp2);
+  var numBucket = List();
 
-  print(intersectionSet);
+  for (var el in integers) {
+    int value = el * 3;
+    numBucket.add(value);
+  }
+
+  for (var num in numBucket) {
+    if (num % 2 == 0) {
+      evenList.add(num);
+    }
+  }
 }
