6038ab88cfa1862ba5f9fc4947fd0b967d4d700b
Ninte <ninte.dev@gmail.com>
2020-12-14 11:57:35 +0100

    added code related to dart's list data structure

--- main.dart
+++ main.dart
@@ -1,6 +1,14 @@
 main() {
-  print("Hello World");
-  int myFirstDartVariable = 5;
-  print("My first Dart variable: $myFirstDartVariable");
-  // print(myFirstDartVariable);
+  // print("Hello World");
+  // int myFirstDartVariable = 5;
+  // print("My first Dart variable: $myFirstDartVariable");
+  // // print(myFirstDartVariable);
+
+  // You have to create and populate a list of cubes given a list of integers.
+  var cubes = [0, 0, 0]; // Modify cubes
+  var integers = List<int>();
+  integers.addAll([1, 2, 3]);
+  cubes.clear();
+  cubes = integers.map((num) => (num * num * num)).toList();
+  print(cubes);
 }
